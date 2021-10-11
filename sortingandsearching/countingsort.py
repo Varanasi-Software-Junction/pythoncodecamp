bb26f6af1293a8d806ed8b6031c267a312b0c67a
Champak's World <champaksworld@gmail.com>
2021-10-11 12:51:14 +0530

    Radix Sort

--- sortingandsearching/countingsort.py
+++ sortingandsearching/countingsort.py
@@ -1,18 +1,23 @@
+def countingSort(a):
+    # print(a)
+    f = [0 for x in range(10)]
+    # print(f)
+    for x in a:
+        f[x] += 1
+    # print(f)
+    for i in range(1, 10):
+        f[i] += f[i - 1]
+    # print(f)
+    output = [0 for x in a]
+    # print(output)
+    for i in reversed(range(len(a))):
+        value = a[i]
+        pos = f[value] - 1
+        output[pos] = value
+        f[value] -= 1
+    return output
+
+
 a = [2, 4, 9, 6, 1, 7, 5, 2, 1, 2]
-print(a)
-f = [0 for x in range(10)]
-print(f)
-for x in a:
-    f[x] += 1
-print(f)
-for i in range(1, 10):
-    f[i] += f[i - 1]
-print(f)
-output = [0 for x in a]
-print(output)
-for i in reversed(range(len(a))):
-    value = a[i]
-    pos = f[value] - 1
-    output[pos] = value
-    f[value] -= 1
+output = countingSort(a)
 print(output)
