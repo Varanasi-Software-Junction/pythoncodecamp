f48444ae70c7476470507ad0ac16d2bcb41d1017
Champak's World <champaksworld@gmail.com>
2021-10-23 13:15:22 +0530

    shareprice

--- shareprice.py
+++ shareprice.py
@@ -1,8 +1,14 @@
-prices = [2, 20, 5, 10, 8, 25, 80]
+prices = [2, 30, 15, 10, 8, 25, 80]
 forwardmaxgain = []
+backwardmaxgain = []
 print(prices)
 n = len(prices)
 for i in range(1, n + 1):
     forwardmaxgain += [max(prices[:i]) - min(prices[:i])]
 
 print(forwardmaxgain)
+for i in range(n, -1, -1):
+    if len(prices[i:]) == 0:
+        continue
+    backwardmaxgain = [max(prices[i:]) - min(prices[i:])] + backwardmaxgain
+print(backwardmaxgain)
