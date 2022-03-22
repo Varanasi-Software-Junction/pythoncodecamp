ab9c13cc36a012e4816b7a9a378d5d2b3ebf3b2d
Champak's World <champaksworld@gmail.com>
2022-03-22 17:49:30 +0530

    Comparisons

---
+++ data types/booleans/comparisons.py
@@ -0,0 +1,19 @@
+"""
+ Comparisons
+There are eight comparison operations in Python.
+They all have the same priority (which is higher than that of the Boolean operations). Comparisons can be chained arbitrarily; for example, x < y <= z is equivalent to x < y and y <= z, except that y is evaluated only once (but in both cases z is not evaluated at all when x < y is found to be false).
+
+This table summarizes the comparison operations:
+
+Operation	Meaning
+<	strictly less than
+<=	less than or equal
+>	strictly greater than
+>=	greater than or equal
+==	equal
+!=	not equal
+is	object identity
+is not	negated object identity
+
+
+"""
--- output.py
+++ output.py
@@ -1,52 +1,16 @@
+def f(x):
+    vowels = ["a", "e", 'i', "o", 'u']
+    x = x.lower()
+    count = 0
+    for ch in x:
+        if ch in vowels:
+            count += 1
+    return count
 
 
-
-
-
-C:\Users\Lenovo\AppData\Local\Microsoft\WindowsApps\python3.10.exe "C:/pythoncodecamp/data types/booleans/booleans.py"
-None False
-True False
-0 False
-0.0 False
-0j False
-'' False
-"" False
-() False
-[] False
-{} False
-A False
-B False
-False
-True
-True
-True
-[] or 0= 0
-{1} or []= {1}
-{1}
-[False]
-False
-True
-True
-True
-[] and 0= []
-{1} and []= []
-[1]
-1
-OR
-Enter A1
-1
-AND
-Enter A0
-0
-
-Process finished with exit code 0
-
-
-
-
-
-
-
-
-
-
+a = ["Python", "Java", "C", "Fortran", "Pascal"]
+for x in a:
+    count = f(x)
+    print(x, count)
+a.sort(key=f)
+print(a)
