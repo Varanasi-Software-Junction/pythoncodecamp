0d78454bcc6b76a60216b3e8d54dcbfadf57f8ea
Champak's World <champaksworld@gmail.com>
2022-04-27 21:26:17 +0530

    Inheritance Explanation

---
+++ OOPs/personemployeemanager.py
@@ -0,0 +1,44 @@
+"""
+Imagine the following scenario.
+
+1. You developed a class called Person with the following data to be used in
+an address book application. We store name, address and mobile numbers in the Person class
+
+2. The next project we are asked to manage Employee details so we plan an Employee class
+The Employee class needs to store name, address, mobileno, post and salary.
+
+The first 3 members are common to both classes. More importantly every Employee is a Person.
+
+This is the exact situation in which inheritance is done.
+
+
+
+
+
+"""
+
+
+class Person:
+    def __init__(self, name, address, mobile):
+        self.name = name
+        self.address = address
+        self.mobile = mobile
+
+    def __str__(self):
+        return "Name={0}, Address={1}, Mobile={2}".format(self.name, self.address, self.mobile)
+
+
+class Employee(Person):  # Extended from Person
+    def __init__(self, name, address, mobile, post, salary):  # Constructor
+        super(Employee, self).__init__(name, address, mobile)  # Super class constructor
+        self.post = post
+        self.salary = salary
+
+    def __str__(self):
+        return "{0}, Post = {1}, Salary={2}".format(super().__str__(), self.post, self.salary)
+
+
+p = Person("Sachin", "Mumbai", "9987887654")
+print(p)
+e = Employee("Sachin", "Mumbai", "9876543212", 'Batsman', 1000)
+print(e)
--- output.py
+++ output.py
@@ -1,13 +1,8 @@
 
 
-
-
-C:\Users\Lenovo\AppData\Local\Microsoft\WindowsApps\python3.10.exe C:/pythoncodecamp/OOPs/oopsinpython.py
-Model=800, Company = Maruti, Price = 100000
-Model=Esteem, Company = Maruti, Price = 1000000
+C:\Users\Lenovo\AppData\Local\Microsoft\WindowsApps\python3.10.exe C:/pythoncodecamp/OOPs/personemployeemanager.py
+Name=Sachin, Address=Mumbai, Mobile=9987887654
+Name=Sachin, Address=Mumbai, Mobile=9876543212, Post = Batsman, Salary=1000
 
 Process finished with exit code 0
-
-
-
 
