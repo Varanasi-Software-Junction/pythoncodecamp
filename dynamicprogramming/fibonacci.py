307c6596f23f31ac55a64a9a93b3dd2cd7de2e63
Champak's World <champaksworld@gmail.com>
2022-05-05 11:33:56 +0530

    DP Fibonacci

--- dynamicprogramming/fibonacci.py
+++ dynamicprogramming/fibonacci.py
@@ -1,4 +1,8 @@
-def fib(n):
+
+
+
+
+def fib(n):#Fib without DP
     global count
     count += 1
     if n == 1:
@@ -8,7 +12,7 @@ def fib(n):
     return fib(n - 1) + fib(n - 2)
 
 
-def dpFib(n):
+def dpFib(n):#Fib with DP
     global count
     global fibs
     count += 1
@@ -20,12 +24,13 @@ def dpFib(n):
 fibs = {}
 
 count = 0
-result = fib(10)
-print(result, end=",")
-print(count)
+result = fib(6)
+print("Fib(6) without Memoization ", result, ", count=", count)
 count = 0
 fibs = {1: 0, 2: 1}
-result = dpFib(10)
-print(result, end=",")
-print(count)
-print(fibs)
+result = dpFib(6)
+print("Fib(6) with Memoization ", result, ", count=", count)
+print("Dictionary as memo ", fibs)
+
+
+
--- output.py
+++ output.py
@@ -1,8 +1,11 @@
 
 
-C:\Users\Lenovo\AppData\Local\Microsoft\WindowsApps\python3.10.exe C:/pythoncodecamp/OOPs/personemployeemanager.py
-Name=Sachin, Address=Mumbai, Mobile=9987887654
-Name=Sachin, Address=Mumbai, Mobile=9876543212, Post = Batsman, Salary=1000
+
+C:\Users\Lenovo\AppData\Local\Microsoft\WindowsApps\python3.10.exe C:/pythoncodecamp/dynamicprogramming/fibonacci.py
+Fib(6) without Memoization  5 , count= 15
+Fib(6) with Memoization  5 , count= 9
+Dictionary as memo  {1: 0, 2: 1, 3: 1, 4: 2, 5: 3, 6: 5}
 
 Process finished with exit code 0
+
 
