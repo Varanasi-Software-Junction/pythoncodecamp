3c8def310deb1b7a12530e8b3b11372498665dfa
Champak's World <champaksworld@gmail.com>
2022-08-23 13:51:56 +0530

    Update WordsInASentence.py

--- dynamicprogramming/WordsInASentence.py
+++ dynamicprogramming/WordsInASentence.py
@@ -1,14 +1,17 @@
-sentence = "This is a train fron Peelikothi to London"
+sentence = "This is a train from Peelikothi to London"
 # print(sentence)
-sentence = " " + sentence.strip().lower() + " "
+sentence = " " + sentence.strip() + " "
 # print(sentence)
-letters = "abcdefhijklmnopqrstuvwxyz"
-start = 0
+letters = "abcdefhijklmnopqrstuvwxyz" + "abcdefhijklmnopqrstuvwxyz".upper()
 n = len(sentence)
-
+startpos = -1
 for i in range(n - 1):
     current = sentence[i]
     next = sentence[i + 1]
     # print(not (current in letters) and next in letters)
     if not (current in letters) and next in letters:
-        print(current, next)
+        print("Start ", current, next, i + 1)
+        startpos = i
+    if not (next in letters) and current in letters:
+        print("End ", current, next, i)
+        print(sentence[startpos:i + 1].strip())
