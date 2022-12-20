03b01cdb1915d88e7f60fe79a952c287d4e97195
Champak's World <champaksworld@gmail.com>
2022-12-20 14:38:15 +0530

    Update Iterative Quicksort.py

--- sortingandsearching/Iterative Quicksort.py
+++ sortingandsearching/Iterative Quicksort.py
@@ -16,27 +16,29 @@ class stack:
     def __str__(self):
         return str(self.a)
 
+
 def quickSort(a):
-    left=0
-    right=len(a)-1
-    st=stack()
-    st.push((left,right))
-    while not  st.isEmpty():
-        left,right=st.pop()
-        if left>=right:
+    left = 0
+    right = len(a) - 1
+    st = stack()
+    st.push((left, right))
+    while not st.isEmpty():
+        left, right = st.pop()
+        if left >= right:
             continue
-        pivot=a[left]
-        finalposition=left
-        for i in range(left+1,right+1):
-            if a[i]>=pivot:
+        pivot = a[left]
+        finalposition = left
+        for i in range(left + 1, right + 1):
+            if a[i] >= pivot:
                 continue
-            finalposition+=1
-            a[i],a[finalposition]=a[finalposition],a[i]
-        a[left],a[finalposition]=a[finalposition],a[left]
-        st.push((left,finalposition-1))
-        st.push((finalposition+1,right))
-a=[9,8,7,6,5,4,3,2,1]
+            finalposition += 1
+            a[i], a[finalposition] = a[finalposition], a[i]
+        a[left], a[finalposition] = a[finalposition], a[left]
+        st.push((left, finalposition - 1))
+        st.push((finalposition + 1, right))
+
+
+a = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 print(a)
 quickSort(a)
 print(a)
-
